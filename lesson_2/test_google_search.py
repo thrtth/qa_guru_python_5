705b3b5823487895e2519bfcb07f29da44db3987
Aleх Bark <thrtth@yandex.ru>
2023-04-06 23:23:02 +0700

    Update test_google_search.py

--- lesson_2/test_google_search.py
+++ lesson_2/test_google_search.py
@@ -7,19 +7,20 @@ from selene import be, have
 def open_browser():
     browser.open('https://google.com')
     browser.driver.set_window_size(1500, 800)
-
-
-@pytest.fixture
-def close_browser():
     yield
     browser.close()
 
+# @pytest.fixture
+# def close_browser():
+#     yield
+#     browser.close()
 
-def test_search_selene(open_browser, close_browser):
+
+def test_search_selene(open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_search_nonexistent(open_browser, close_browser):
+def test_search_nonexistent(open_browser):
     browser.element('[name="q"]').should(be.blank).type('jahsdguywrtwegajhsdgfuye').press_enter()
-    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0' or 'About 0 results'))
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
